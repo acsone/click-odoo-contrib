b7c1e1d908f52dfebad0bdb1c6094b63eb26c75a
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-05-26 11:57:42 +0200

    Add method to find dependencies from manifest

--- click_odoo_contrib/manifest.py
+++ click_odoo_contrib/manifest.py
@@ -4,10 +4,16 @@
 import ast
 import os
 
+from click_odoo import odoo
+
 MANIFEST_NAMES = ('__manifest__.py', '__openerp__.py')
 
 
 class NoManifestFound(Exception):
+    pass
+
+
+class ModuleNotFound(Exception):
     pass
 
 
@@ -41,3 +47,49 @@ def find_addons(addons_dir, installable_only=True):
         if installable_only and not manifest.get('installable', True):
             continue
         yield addon_name, addon_dir, manifest
+
+
+def expand_dependencies(module_names, include_auto_install=False):
+    """ Return a set of module names with their transitive
+    dependencies.  This method does not need an Odoo database,
+    but requires the addons path to be initialized.
+
+    Caveat: auto_install modules are not added automatically.
+    """
+
+    def add_deps(name):
+        if name in res:
+            return
+        res.add(name)
+        path = odoo.modules.get_module_path(name)
+        if not path:
+            raise ModuleNotFound(name)
+        manifest = read_manifest(path)
+        for dep in manifest.get('depends', ['base']):
+            add_deps(dep)
+
+    res = set()
+    for module_name in module_names:
+        add_deps(module_name)
+    if include_auto_install:
+        auto_install_list = []
+        for module_name in sorted(odoo.modules.module.get_modules()):
+            module_path = odoo.modules.get_module_path(module_name)
+            manifest = read_manifest(module_path)
+            if manifest.get('auto_install'):
+                auto_install_list.append((module_name, manifest))
+        retry = True
+        while retry:
+            retry = False
+            for module_name, manifest in auto_install_list:
+                if module_name in res:
+                    continue
+                depends = set(manifest.get('depends', ['base']))
+                if depends.issubset(res):
+                    # all dependencies of auto_install module are
+                    # installed so we add it
+                    add_deps(module_name)
+                    # retry, in case an auto_install module depends
+                    # on other auto_install modules
+                    retry = True
+    return res
--- tests/test_manifest.py
+++ tests/test_manifest.py
@@ -21,3 +21,21 @@ def test_manifest_find_addons_uninstallable():
     assert len(addons) == 2
     assert addons[0][0] == 'addon1'
     assert addons[1][0] == 'addon_uninstallable'
+
+
+def test_manifest_expand_dependencies():
+    res = manifest.expand_dependencies(['contacts', 'base_import'])
+    assert 'contacts' in res
+    assert 'base_import' in res
+    assert 'base' in res  # obviously
+    assert 'web' in res  # base_import depends on it
+    assert 'auth_crypt' not in res
+
+
+def test_manifest_expand_dependencies_auto_install():
+    res = manifest.expand_dependencies(['contacts'], include_auto_install=True)
+    assert 'contacts' in res
+    assert 'base' in res  # obviously
+    assert 'auth_crypt' in res  # web is autoinstall
+    assert 'web' in res  # web is autoinstall
+    assert 'base_import' in res  # base_import is indirect autoinstall
