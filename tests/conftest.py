f4568f2ddd1023b1b1b53ac0b159f0e35d08b81c
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-06-12 00:08:57 +0200

    makepot: fix Odoo < 11 tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 # Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+import os
 import subprocess
+import textwrap
 
 import pytest
 
@@ -13,15 +15,24 @@ from click_odoo import odoo, odoo_bin
 odoo.netsvc._logger_init = True
 
 
-def _init_odoo_db(dbname):
+def _init_odoo_db(dbname, test_addons_dir=None):
     subprocess.check_call([
         'createdb', dbname,
     ])
-    subprocess.check_call([
+    cmd = [
         odoo_bin,
         '-d', dbname,
         '--stop-after-init',
-    ])
+    ]
+    if test_addons_dir:
+        addons_path = [
+            os.path.join(odoo.__path__[0], 'addons'),
+            os.path.join(odoo.__path__[0], '..', 'addons'),
+            test_addons_dir,
+        ]
+        cmd.append('--addons-path')
+        cmd.append(','.join(addons_path))
+    subprocess.check_call(cmd)
 
 
 def _drop_db(dbname):
@@ -31,11 +42,29 @@ def _drop_db(dbname):
 
 
 @pytest.fixture(scope='module')
-def odoodb():
+def odoodb(request):
     dbname = 'click-odoo-contrib-test-{}'.\
         format(odoo.release.version_info[0])
-    _init_odoo_db(dbname)
+    test_addons_dir = getattr(request.module, 'test_addons_dir', '')
+    _init_odoo_db(dbname, test_addons_dir)
     try:
         yield dbname
     finally:
         _drop_db(dbname)
+
+
+@pytest.fixture(scope='function')
+def odoocfg(request, tmpdir):
+    addons_path = [
+        os.path.join(odoo.__path__[0], 'addons'),
+        os.path.join(odoo.__path__[0], '..', 'addons'),
+    ]
+    test_addons_dir = getattr(request.module, 'test_addons_dir', '')
+    if test_addons_dir:
+        addons_path.append(test_addons_dir)
+    odoo_cfg = tmpdir / 'odoo.cfg'
+    odoo_cfg.write(textwrap.dedent("""\
+        [options]
+        addons_path = {}
+    """.format(','.join(addons_path))))
+    yield odoo_cfg
--- tests/test_makepot.py
+++ tests/test_makepot.py
@@ -2,49 +2,38 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 import os
-import click_odoo
 import subprocess
-import textwrap
 
 from click.testing import CliRunner
+import click_odoo
 
 from click_odoo_contrib.makepot import main
 
-ADDONS_DIR = os.path.join(
+# this extends the addons path of the odoodb and odoocfg fixtures
+test_addons_dir = os.path.join(
     os.path.dirname(__file__), 'data', 'test_makepot')
 
-ADDONS_PATH = ','.join([
-    os.path.join(click_odoo.odoo.__path__[0], 'addons'),
-    os.path.join(click_odoo.odoo.__path__[0], '..', 'addons'),
-    ADDONS_DIR,
-])
 
-
-def test_makepot(odoodb, tmpdir):
-    odoo_cfg = tmpdir / 'odoo.cfg'
-    odoo_cfg.write(textwrap.dedent("""\
-        [options]
-        addons_path = {}
-    """.format(ADDONS_PATH)))
+def test_makepot(odoodb, odoocfg, tmpdir):
     addon_name = 'addon_test_makepot'
 
     subprocess.check_call([
         click_odoo.odoo_bin,
-        '-d', str(odoodb),
-        '-c', str(odoo_cfg),
+        '-d', odoodb,
+        '-c', str(odoocfg),
         '-i', addon_name,
         '--stop-after-init',
     ])
 
     result = CliRunner().invoke(main, [
         '-d', odoodb,
-        '-c', str(odoo_cfg),
-        '--addons-dir', ADDONS_DIR,
+        '-c', str(odoocfg),
+        '--addons-dir', test_addons_dir,
     ])
 
     assert result.exit_code == 0
 
-    addon_path = os.path.join(ADDONS_DIR, addon_name)
+    addon_path = os.path.join(test_addons_dir, addon_name)
     i18n_path = os.path.join(addon_path, 'i18n')
     pot_filepath = os.path.join(i18n_path, addon_name + '.pot')
 
