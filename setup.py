7a8148343831365dd33d92f2435164c2c6f927ef
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-10-06 17:36:11 +0200

    Explicit python 3.7 and Odoo 12 support

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Odoo',
     ],
     entry_points='''
