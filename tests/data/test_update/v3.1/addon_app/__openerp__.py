987933041cec98bd89c85ffc80c265f051170071
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2020-12-29 17:57:52 +0100

    update: Don't update uninstallable addons

---
+++ tests/data/test_update/v3.1/addon_app/__openerp__.py
@@ -0,0 +1,7 @@
+{
+    "name": "addon_app",
+    "application": True,
+    "version": "3.1",
+    "depends": ["addon_d1"],
+    "description": "Add two dependencie.",
+}
---
+++ tests/data/test_update/v3.1/addon_d2/__openerp__.py
@@ -0,0 +1,6 @@
+{
+    "name": "addon_d2",
+    "version": "3.0",
+    "installable": False,
+    "data": ["doesnotexist.xml"],
+}
