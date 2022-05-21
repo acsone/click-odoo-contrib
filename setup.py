8302098d358863f10f2f52311bf0ab95aeaf2da3
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2022-05-21 22:56:31 +0200

    Fix classifiers

--- setup.py
+++ setup.py
@@ -34,10 +34,9 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: "
         "GNU Lesser General Public License v3 or later (LGPLv3+)",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Framework :: Odoo",
     ],
     entry_points="""
