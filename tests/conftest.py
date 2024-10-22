37d0827e61973ad484b7a53b759dbbc4dc8635c9
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2024-10-22 21:54:22 +0200

    Fix logger interaction with test runner in Odoo 18

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ from click_odoo import odoo, odoo_bin
 # This hack is necessary because the way CliRunner patches
 # stdout is not compatible with the Odoo logging initialization
 # mechanism. Logging is therefore tested with subprocesses.
-odoo.netsvc._logger_init = True
+odoo.netsvc.init_logger = lambda: None
 
 
 def _init_odoo_db(dbname, test_addons_dir=None):
