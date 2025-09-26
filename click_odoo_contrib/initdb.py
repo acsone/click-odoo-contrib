321f5b05895a224a2813a2d9af86c25b56518fcc
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2025-09-26 18:13:22 +0200

    click-odoo-initdb: rework to support Odoo 19

--- click_odoo_contrib/initdb.py
+++ click_odoo_contrib/initdb.py
@@ -62,16 +62,21 @@ def _patch_ir_attachment_store(force_db_storage):
 def odoo_createdb(dbname, demo, module_names, force_db_storage):
     with _patch_ir_attachment_store(force_db_storage):
         odoo.service.db._create_empty_database(dbname)
-        odoo.tools.config["init"] = dict.fromkeys(module_names, 1)
         if odoo.release.version_info >= (19, 0):
             odoo.tools.config["with_demo"] = demo
             odoo.modules.registry.Registry.new(
-                dbname, new_db_demo=demo, update_module=True
+                dbname,
+                new_db_demo=demo,
+                update_module=True,
+                install_modules=module_names,
             )
         else:
             odoo.tools.config["without_demo"] = not demo
+            odoo.tools.config["init"] = dict.fromkeys(module_names, 1)
             odoo.modules.registry.Registry.new(
-                dbname, force_demo=demo, update_module=True
+                dbname,
+                force_demo=demo,
+                update_module=True,
             )
         _logger.info(click.style(f"Created new Odoo database {dbname}.", fg="green"))
         with odoo.sql_db.db_connect(dbname).cursor() as cr:
